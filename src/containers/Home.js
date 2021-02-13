9b1d6cda410d26a443b5e05d5bb9bde57b9e491f
Jared Seefried <jared.seefried@yahoo.com>
2021-02-13 13:17:32 -0700

    Created Home Page and routes file

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>E-Commerce Site</h1>
+        <p className="text-muted">A place to sell something</p>
+      </div>
+    </div>
+  );
+}
